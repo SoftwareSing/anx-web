5e0ae3cb9a4247b67292875fc52a49c80b37f00b
RayFV <rayfvfree@outlook.com>
2019-02-11 16:57:48 +0800

    Added Image Slider

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from 'client/app/app-routing.module';
 import { AppComponent } from 'client/app/app.component';
-import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './navbar/navbar.component';
+
+import { CoursesComponent } from 'client/app/courses/courses.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 import { HomeLayoutComponent } from './layout/home-layout/home-layout.component';
 import { ManagementLayoutComponent } from './layout/management-layout/management-layout.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { ManagementLayoutComponent } from './layout/management-layout/management
     NavbarComponent,
     FooterComponent,
     HomeLayoutComponent,
-    ManagementLayoutComponent
+    ManagementLayoutComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
--- client/app/home/home.component.ts
+++ client/app/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Image } from '../SilderImage';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
 
+export class HomeComponent implements OnInit {
+  images: Image[];
   constructor() { }
 
   ngOnInit() {
+    this.images = [
+      { title: 'Slide 1', url: 'assets/slider/img/banner0.jpg' },
+      { title: 'Slide 2', url: 'assets/slider/img/banner2.jpg' },
+      { title: 'Slide 3', url: 'assets/slider/img/banner3.jpg' },
+      { title: 'Slide 4', url: 'assets/slider/img/banner4.jpg' },
+      { title: 'Slide 5', url: 'assets/slider/img/banner5.jpg' }
+    ];
   }
 
 }
