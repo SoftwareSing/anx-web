684ca24d4b2a0e411e217f6b19f15334586c0720
RayFV <rayfvfree@outlook.com>
2019-02-14 14:55:58 +0800

    Update Navbar
    1. Changed href to routerLink
    2. 新增logo，將原本的nav圖片改為logo+背景漸層

--- client/app/app-routing.module.ts
+++ client/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { CoursesComponent } from 'client/app/courses/courses.component';
 import { HomeComponent } from './home/home.component';
 import { HomeLayoutComponent } from './layout/home-layout/home-layout.component';
 import { ManagementLayoutComponent } from './layout/management-layout/management-layout.component';
@@ -10,7 +11,8 @@ const routes: Routes = [
     path: '',
     component: HomeLayoutComponent,
     children: [
-      {path: '', component: HomeComponent}
+      {path: '', component: HomeComponent},
+      {path: 'courses', component: CoursesComponent}
     ]
   },
   {
