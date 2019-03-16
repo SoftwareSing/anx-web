aa92b6ca21338dc89979cb8f8cebd34e4f3f245d
SoftwareSing <SoftwareSing@gmail.com>
2019-03-16 16:31:35 +0800

    使用 tslint --fix 修正不符合規則的code

--- client/src/app/model/course.ts
+++ client/src/app/model/course.ts
@@ -3,11 +3,11 @@
 */
 
 export class Course {
-    name: string;
-    startDate: string;
-    endDate: string;
-    comfirmDate: string;
-    state: string;
-    click: number;
-    location: string;
+  name: string;
+  startDate: string;
+  endDate: string;
+  comfirmDate: string;
+  state: string;
+  click: number;
+  location: string;
 }
